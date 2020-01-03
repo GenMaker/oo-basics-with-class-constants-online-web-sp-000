776bad68ca5aaa609834c0323f0c1e059070445c
Panela Leung <leungpanela@gmail.com>
2020-01-03 20:56:23 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,10 +7,10 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    # if BRANDS.include?(brand)
-    # else
-    #   BRANDS << brand
-    #end
+    if BRANDS.include?(brand)
+    else
+      BRANDS << brand
+    end
 
 
   end
